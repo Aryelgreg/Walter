6178610d6191d4198f30cfde25e726d3a90e9d6c
Aryelgreg <aryelgregorio@gmail.com>
2023-09-19 11:05:46 -0400

    Infinity scroll para frente e para trás

--- Walter/Assets/Sprites/BackGorund Industrial/Parallax.cs
+++ Walter/Assets/Sprites/BackGorund Industrial/Parallax.cs
@@ -6,12 +6,6 @@ public class EfeitoParallax : MonoBehaviour
     [SerializeField] private Image fundo;
     [SerializeField] private float velocidade;
     [SerializeField] private float larguraDaImagem; // A largura da imagem de fundo
-    private float limiteDireita; // A posição x onde a imagem atinge o limite direito
-
-    private void Start()
-    {
-        limiteDireita = larguraDaImagem / 2; // Define o limite direito
-    }
 
     private void Update()
     {
@@ -24,10 +18,17 @@ public class EfeitoParallax : MonoBehaviour
         transform.position = new Vector3(transform.position.x - velocidade * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
 
         // Verifica se a imagem de fundo se moveu completamente para a esquerda
-        if (transform.position.x < -limiteDireita)
+        if (transform.position.x < -larguraDaImagem / 2)
         {
-            // Reposiciona a imagem de fundo para o limite direito
-            float newX = limiteDireita + (transform.position.x + limiteDireita) % larguraDaImagem;
+            // Reposiciona a imagem de fundo para criar o efeito de scroll infinito
+            float newX = larguraDaImagem / 2 + (transform.position.x + larguraDaImagem / 2) % larguraDaImagem;
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+        // Verifica se a imagem de fundo se moveu completamente para a direita
+        else if (transform.position.x > larguraDaImagem / 2)
+        {
+            // Reposiciona a imagem de fundo para criar o efeito de scroll infinito
+            float newX = -larguraDaImagem / 2 + (transform.position.x - larguraDaImagem / 2) % larguraDaImagem;
             transform.position = new Vector3(newX, transform.position.y, transform.position.z);
         }
     }
