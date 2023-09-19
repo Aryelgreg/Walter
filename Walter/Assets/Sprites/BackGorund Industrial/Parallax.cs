e3e690bed31a2c6f2b7fa023c4b04b5204096b45
Aryelgreg <aryelgregorio@gmail.com>
2023-09-19 10:43:42 -0400

    Parallax com scroll infinito
    finalmente scroll infinito

--- Walter/Assets/Sprites/BackGorund Industrial/Parallax.cs
+++ Walter/Assets/Sprites/BackGorund Industrial/Parallax.cs
@@ -5,6 +5,13 @@ public class EfeitoParallax : MonoBehaviour
 {
     [SerializeField] private Image fundo;
     [SerializeField] private float velocidade;
+    [SerializeField] private float larguraDaImagem; // A largura da imagem de fundo
+    private float limiteDireita; // A posição x onde a imagem atinge o limite direito
+
+    private void Start()
+    {
+        limiteDireita = larguraDaImagem / 2; // Define o limite direito
+    }
 
     private void Update()
     {
@@ -13,6 +20,15 @@ public class EfeitoParallax : MonoBehaviour
 
     public void MoveFundo()
     {
+        // Move a imagem de fundo
         transform.position = new Vector3(transform.position.x - velocidade * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+
+        // Verifica se a imagem de fundo se moveu completamente para a esquerda
+        if (transform.position.x < -limiteDireita)
+        {
+            // Reposiciona a imagem de fundo para o limite direito
+            float newX = limiteDireita + (transform.position.x + limiteDireita) % larguraDaImagem;
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
     }
 }
