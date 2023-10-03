ab0e8241fcd578680abfb3567b87d225721f9ac8
Aryelgreg <aryelgregorio@gmail.com>
2023-10-03 07:49:27 -0400

    hehe

--- Walter/Walter/Assets/Sprites/BackGorund Industrial/Parallax.cs
+++ Walter/Walter/Assets/Sprites/BackGorund Industrial/Parallax.cs
@@ -16,5 +16,20 @@ public class EfeitoParallax : MonoBehaviour
     {
         // Move a imagem de fundo
         transform.position = new Vector3(transform.position.x - velocidade * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+
+        // Verifica se a imagem de fundo se moveu completamente para a esquerda
+        if (transform.position.x < -larguraDaImagem / 2)
+        {
+            // Reposiciona a imagem de fundo para criar o efeito de scroll infinito
+            float newX = larguraDaImagem / 2 + (transform.position.x + larguraDaImagem / 2) % larguraDaImagem;
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+        // Verifica se a imagem de fundo se moveu completamente para a direita
+        else if (transform.position.x > larguraDaImagem / 2)
+        {
+            // Reposiciona a imagem de fundo para criar o efeito de scroll infinito
+            float newX = -larguraDaImagem / 2 + (transform.position.x - larguraDaImagem / 2) % larguraDaImagem;
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
     }
 }
