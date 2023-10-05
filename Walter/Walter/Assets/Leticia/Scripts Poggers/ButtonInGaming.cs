6d0e6321561c2d80919759917d2972fcad0e9c04
leticia426 <145141801+leticia426@users.noreply.github.com>
2023-10-05 09:59:45 -0300

    Atualização1
    Primeira fase e menu

---
+++ Walter/Walter/Assets/Leticia/Scripts Poggers/ButtonInGaming.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonInGaming : MonoBehaviour
+{
+    [SerializeField] private Button ButtonRestart;
+    [SerializeField] private Button ButtonMenu;
+    [SerializeField] private string RecomecarFase;
+    [SerializeField] private string VoltarAoMenu;
+
+    private void Awake()
+    {
+        ButtonRestart.onClick.AddListener(OnButtonRestartClick);
+        ButtonMenu.onClick.AddListener(OnButtonMenuClick);
+    }
+
+    private void OnButtonRestartClick()
+    {
+        // Depois configurar a opção de áudio
+        Restart();
+    }
+
+    private void OnButtonMenuClick()
+    {
+        // Depois configurar a opção de áudio
+        Menu();
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(RecomecarFase);
+    }
+
+    private void Menu()
+    {
+        SceneManager.LoadScene(VoltarAoMenu);
+    }
+}
